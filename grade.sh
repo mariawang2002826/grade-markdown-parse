8204dc3ec55f695a8d78e04410b1fea261352457
mariawang2002826 <mariawang2002@gmail.com>
2022-02-10 12:22:10 -0800

    Optimize gradescope tests

--- grade.sh
+++ grade.sh
@@ -1,6 +1,7 @@
 
 git clone $1
-cp GradingTests.java markdown-parse/
-cd markdown-parse
-javac -cp lib/*:. GradingTests.java MarkdownParse.java
-java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
+if test -f "./markdown-parse/MarkdownParse.java"; then
+    cp ./markdown-parse/MarkdownParse.java .
+    make grade
+    rm -rf ./markdown-parse
+fi
